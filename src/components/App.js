176785cdb8b63e29460c35c61e3142dfa4d681b0
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-10-05 16:04:46 +0100

    update tests to match snapshots

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Search from "./Search";
 
 function App() {
   return (
-    <div className="App" data-testid="app">
+    <div className="App">
       <img
         src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
         alt="NASA logo"
