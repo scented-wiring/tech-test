d888b60f6865fe2dcc757c08e973b0ca9b07a390
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-10-06 18:34:21 +0100

    update SearchResults component test

--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -3,9 +3,10 @@ import { render } from "@testing-library/react";
 import SearchResults from "../components/SearchResults";
 
 describe("SearchResults", () => {
-  const { asFragment } = render(<SearchResults />);
+  const mockResults = [{ img: "image1" }];
 
   it("renders correctly", () => {
+    const { asFragment } = render(<SearchResults results={mockResults} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
